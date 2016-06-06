35621f168f23cfa812628962b2dd35a242ccb2bb
James Dalrymple <jamesdalrymple@hotmail.com>
2016-06-06 14:33:37 -0400

    consumer initials: regex could be improved

--- PI/Dup Adms/Code/2 modify dup_adm.R
+++ PI/Dup Adms/Code/2 modify dup_adm.R
@@ -6,12 +6,19 @@ staff <- copy(sql$output$staff)
 setf(adm, j = Cs(cmh_effdt, team_effdt), as.Date)
 adm[, team_short := cmh_recode(team)]
 
+adm
+grep(pattern = "[[:alpha:+], :alpha:{0}]", replacement = "", x = "last, first", perl = FALSE)
+gsub(pattern = "[[:alpha:+], :alpha:{0}]", replacement = "", x = "last, first", perl = FALSE)
+
+aux$initials <- function(x) {
+  first_i <- str_match(string = x, pattern = "[:alpha:]+, ([:alpha:]{1})")[, 2]
+  last_i = substr(x, 1, 1)
+  toupper(paste0(first_i, last_i))
+}
+setf(adm, j = "client_name", initials)
 adm[, active_teams := .N, by = case_no]
-
-
 adm <- adm[active_teams > 1]
 
 adm[staff[primary_staff_or_not == "Y"],
     Cs(prim_staff, supervisor) := list(i.assigned_staff, i.supervisor),
     on = "case_no"]
-
