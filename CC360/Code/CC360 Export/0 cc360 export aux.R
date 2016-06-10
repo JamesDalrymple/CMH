33536ec38684a7fa16ac9181ac126c8f3fa97606
James Dalrymple <jamesdalrymple@hotmail.com>
2016-06-10 16:18:49 -0400

    sql_joins.R WIP

--- CC360/Code/CC360 Export/0 cc360 export aux.R
+++ CC360/Code/CC360 Export/0 cc360 export aux.R
@@ -1,7 +1,7 @@
 ### fund only auxillary ###
 options(java.parameters = "- Xmx1024m")
 pkg_loader(packages = c("gdata", "data.table", "zoo", "xlsx", "RODBC",
-  "SASxport", "ReporteRs", "ggplot2", "plyr", "RColorBrewer"),
+  "SASxport", "ReporteRs", "ggplot2", "plyr", "RColorBrewer", "sqldf"),
   repos = "https://cran.mtu.edu/")
 aux <- new.env(parent = .GlobalEnv)
 
---
+++ CC360/Code/CC360 Export/4 cc360 export sql_joins.R
@@ -0,0 +1,48 @@
+# cohorts ---------------------------------------------------------------------
+# pre/post HH is a primary grouping
+# pre/post HH without nurse vs HH with nurse SHOULD be considered
+
+hh_tier <- copy(modify$tiers)
+hh_services <- copy(modify$hh_services)
+hh_detail <- copy(modify$hh_detail)
+cc360_main <- copy(modify$cc360_main)
+cc360_med <- copy(modify$cc360_med)
+hh_bucket <- copy(modify$hh_bucket)
+
+
+sqldf("select
+  case_no, min(team_effdt) as first_hh_dt
+      from hh_detail
+      group by case_no", drv = "SQLite")
+
+
+unique(
+data.table(
+sqldf("select
+  first.case_no, first.first_hh_dt,
+  strftime('%m-%Y', first.first_hh_dt) AS month,
+  tier.util_level, tier.tier, tier.num_ER_last180,
+  tier.num_PH_IPs_last180, tier.num_ER_or_PH_IP_last180,
+  hh_adm.gender, hh_adm.MI, hh_adm.DD,
+  case when nurse.hh_nurse is null then 'N' else 'Y' end as hh_nurse_ever,
+  bkt.hh_bucket
+  from (select
+  first_hh.case_no, min(first_hh.team_effdt) as first_hh_dt
+  from hh_detail as first_hh
+  group by first_hh.case_no) as first
+  left join hh_tier as tier on
+    first.case_no = tier.case_no and
+    strftime('%m-%Y', first.first_hh_dt) = strftime('%m-%Y', tier.end_date)
+  left join hh_detail as hh_adm on
+    hh_adm.case_no = first.case_no and
+    hh_adm.team_effdt = first.first_hh_dt
+  left join hh_detail as nurse on
+    nurse.case_no = first.case_no and nurse.hh_nurse = 'Y' and
+    hh_adm.team_effdt = first.first_hh_dt
+  left join hh_bucket as bkt on
+    first.case_no = bkt.case_no
+    ", drv = "SQLite")
+))
+
+
+modify$hh_detail[, list(first_hh_date = min(team_effdt)), by = case_no]
